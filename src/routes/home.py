1d437bd2b0d3174b88bf2dac5c06bd03e051bf9a
Rodrigo Gómez Maitret <green.tie.rgm@gmail.com>
2022-10-13 14:14:53 -0500

    Token.

--- src/routes/home.py
+++ src/routes/home.py
@@ -1,7 +1,9 @@
 from flask import jsonify
 from src import app
+from src.services.access import token_required
 from src.services.hello import say_hello
 
 @app.route('/')
+@token_required
 def home():
     return jsonify({'message': say_hello()})
